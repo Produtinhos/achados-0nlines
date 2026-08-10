d10faf112d48606f74fdffddeb0cda6080cbde0f
Achados Online <ferreiraelias608@gmail.com>
2026-08-10 15:03:42 -0300

    Add 'Cama, Mesa e Banho' to categories list

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const products = [
   }
 ];
 
-const categories = ["Todos", "Casa", "Cozinha", "Eletrônicos", "Utilidades", "Masculino", "Feminino"];
+const categories = ["Todos", "Casa", "Cozinha", "Eletrônicos", "Utilidades", "Masculino", "Feminino", "Cama, Mesa e Banho"];
 let activeCategory = "Todos";
 
 const productsEl = document.querySelector("#products");
